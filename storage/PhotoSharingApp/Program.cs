abc45344f72750d5aad4f6d06eef79236fbddc89
Fergal <fnealon@gmail.com>
2020-04-13 11:05:02 +0100

    Storage lab

---
+++ storage/PhotoSharingApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        private static CloudStorageAccount storageAccount;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration["StorageAccountConnectionString"];
+            if (!CloudStorageAccount.TryParse(connectionString, out CloudStorageAccount storageAccount))
+            {
+                Console.WriteLine("Unable to parse connection string");
+                return;
+            }
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("photoblobs");
+            bool created = await blobContainer.CreateIfNotExistsAsync();
+            Console.WriteLine(created ? "Created the Blob container" : "Blob container already exists.");
+        }
+    }
+}
